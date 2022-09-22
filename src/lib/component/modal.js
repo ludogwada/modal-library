14bc1e5d3e43bf98eb3c8a8dd80348ad7d0ea1e2
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-09-22 08:47:21 -0400

    feat(modal): add modal code

---
+++ src/lib/component/modal.js
@@ -0,0 +1,39 @@
+import React, { useCallback, useEffect } from 'react';
+
+import './ModalLibrary.css';
+
+const Modal = (props) => {
+	const keyPress = useCallback(
+		(e) => {
+			if (e.key === 'Escape' && props.showModal) {
+				props.setShowModal(false);
+			}
+		},
+		[props]
+	);
+
+	useEffect(() => {
+		document.addEventListener('keydown', keyPress);
+		return () => document.removeEventListener('keydown', keyPress);
+	}, [keyPress]);
+
+	return (
+		<>
+			{props.showModal ? (
+				<section className='container-modal'>
+					<div className='modal'>
+						<span className='modal-text'>{props.content}</span>
+						<button
+							className='modal-cross'
+							aria-label='Close modal'
+							onClick={() => props.setShowModal(false)}>
+							X
+						</button>
+					</div>
+				</section>
+			) : null}
+		</>
+	);
+};
+
+export default Modal;
